875a4c220d75d027a95dab48087fbb58f23d268e
akchabekova <Luelum7@gmail.com>
2022-01-25 21:25:31 +0100

    start

---
+++ script/script.js
@@ -0,0 +1,111 @@
+let num = 25
+console.log(num)
+
+let a = 10
+let b = 2
+let c = 5
+console.log(a + b + c)
+
+let x = 100
+console.log( x * 2)
+
+let y = 67 % 7
+console.log( y )
+
+ a = 10
+ b = 2
+ console.log( a + b)
+ console.log( a - b)
+ console.log( a * b)
+ console.log( a / b)
+
+  c = 15
+  let d = 2
+  let result = ( c + d)
+  console.log( result )
+
+ a = 30
+ b = 15
+ c = (( a + b) % 4)
+ console.log( c )
+
+ a = 17
+ b = 10
+ c = ( a - b )
+ d = 20
+ result = ( c + d )
+ console.log( result )
+
+a = '10'
+b = '99'
+result = ( 'a' + 'b' )
+console.log( result)
+
+let name = 'Аскар'
+let number = 10
+result = (name + number)
+console.log( result)
+
+a = 20
+b = 10
+b = ( a - b )
+console.log( b )
+
+a = 90
+a = ( a + 2)
+console.log( a )
+
+a = 50
+a = ( 50 * 5)
+console.log( a )
+
+a = 4
+b = 2
+result = ( a % b )
+console.log( result )
+
+a = 11
+b = 4
+result = ( a % b)
+console.log( result )
+
+a = 20
+b = 21
+console.log( a > b )
+
+a = 20
+b = 21
+console.log( a < b )
+
+name = "Aiganysh"
+console.log( name )
+
+age = 26
+console.log( age )
+
+let str = 'Привет, Мир!'
+console.log( str )
+
+let str1 = 'Привет,'
+let str2 = ' Мир!'
+console.log( str1 + str2 )
+
+name = ' Айганыш!'
+console.log( str1 + name )
+
+
+str1 = 'Мне '
+str2 = ' лет!'
+console.log( str1 + age + str2 )
+
+str1 = 'Welcome, '
+str2 = 'Ivan!'
+console.log( str1 + str2 )
+
+
+str1 = '365 '
+str2 = 'Земля '
+str3 = '7 млдр. '
+str4 = 'Солнце.'
+result = ("В нашем году "  + str1+  "дней. " + " Днём у нас светит " + str4 + " Население планеты "+ str2+ "составляет примерно " + str4)
+console.log( result )
